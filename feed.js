821d3ff145226feaea0c9465cc24234cdd3650be
LatencyVariation <reynoldsa179@gmail.com>
2022-07-06 18:12:21 +0100

    Added Home Feed
    Feed that displays posts from a local api on the Home Page.

---
+++ feed.js
@@ -0,0 +1,44 @@
+function populateFeed(){
+    const feed = document.getElementById("feed");
+    var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = () => {
+          if (xhr.readyState == 4) {
+              if (xhr.status == 200) {
+                var postList = JSON.parse(xhr.responseText);
+                feed.innerHTML = "";
+                for (var i = 0, len = postList.length; i < len; ++i) {
+                  var currentValue = postList[i];
+                  var currentTitle = currentValue.title;
+                  var currentContent = currentValue.content;
+  
+                  var postBox = document.createElement("div");
+                  postBox.setAttribute("class", "post");
+                  var postMeta = document.createElement("div");
+                  postMeta.setAttribute("class", "post-meta");
+                  var postTime = document.createElement("h3");
+                  postTime.innerHTML = currentValue.time;
+                  var postAuthor = document.createElement("h3");
+                  postAuthor.innerHTML = `Posted by ${currentValue.author}`;
+
+                  var postTitle = document.createElement("h2");
+                  postTitle.innerHTML = currentTitle;
+                  var postContent = document.createElement("p");
+                  postContent.setAttribute("class", "post-content");
+                  postContent.innerHTML = currentContent;
+                  
+                  postBox.appendChild(postMeta);
+                  postMeta.appendChild(postTime);
+                  postMeta.appendChild(postAuthor);
+                  postBox.appendChild(postTitle);
+                  postBox.appendChild(postContent);
+                  feed.appendChild(postBox);
+                }
+              }
+          }
+      }
+    xhr.open("GET", `api/posts.json`, true);
+    xhr.send();
+  }
+  
+  populateFeed();
+  setInterval(() => populateFeed(), 10000)
